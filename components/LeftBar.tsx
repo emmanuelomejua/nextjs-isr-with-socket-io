f1a809f33f359c1bf4023473d86d6c9d2172a1a3
Emmanuel Omejua <omejuaifeanyi2@gmail.com>
2025-07-21 04:06:46 +0000

    sidebar design completed

--- components/LeftBar.tsx
+++ components/LeftBar.tsx
@@ -1,8 +1,107 @@
-import React from 'react'
+import Link from "next/link";
+import Image from 'next/image'
+
+const menuList = [
+  {
+    id: 1,
+    name: "Homepage",
+    link: "/",
+    icon: "home.svg",
+  },
+  {
+    id: 2,
+    name: "Explore",
+    link: "/",
+    icon: "explore.svg",
+  },
+  {
+    id: 3,
+    name: "Notification",
+    link: "/",
+    icon: "notification.svg",
+  },
+  {
+    id: 4,
+    name: "Messages",
+    link: "/",
+    icon: "message.svg",
+  },
+  {
+    id: 5,
+    name: "Bookmarks",
+    link: "/",
+    icon: "bookmark.svg",
+  },
+  {
+    id: 6,
+    name: "Jobs",
+    link: "/",
+    icon: "job.svg",
+  },
+  {
+    id: 7,
+    name: "Communities",
+    link: "/",
+    icon: "community.svg",
+  },
+  {
+    id: 8,
+    name: "Premium",
+    link: "/",
+    icon: "logo.svg",
+  },
+  {
+    id: 9,
+    name: "Profile",
+    link: "/",
+    icon: "profile.svg",
+  },
+  {
+    id: 10,
+    name: "More",
+    link: "/",
+    icon: "more.svg",
+  },
+];
 
 const LeftBar = () => {
   return (
-    <div>LeftBar</div>
+    <div className='h-[max-content] sticky top-0 flex flex-col justify-between pt-2 pb-4'>
+      <div className="flex flex-col gap-4 text-lg items-center xxl:items-start">
+        <Link href='/' className="p-2 rounded-full hover:bg-[#181818]">
+          <Image src='icons/logo.svg' alt="Logo" width={24} height={24}/>
+        </Link>
+
+        <div className="flex flex-col gap-1">
+          {menuList.map((list, index) => (
+            <Link href={list.link} key={index} className="p-2 rounded-full hover:bg-[#181818] flex items-center gap-4">
+              <Image src={`icons/${list.icon}`} alt="" height={14} width={14}/>
+              <span className="hidden xxl:inline text-sm">{list.name}</span>
+            </Link>
+          ))}
+
+        <Link
+          href="/compose/post"
+          className="hidden xxl:block bg-white text-black rounded-full font-bold py-2 px-20"
+        >
+          Post
+        </Link>
+        </div>
+      </div>
+
+      <div className="flex items-center justify-between mt-4">
+        <div className="flex items-center gap-2">
+          <div className="w-10 h-10 relative rounded-full overflow-hidden">
+             <Image  src="/general/avatar.png" alt="lama dev" fill={true} />
+          </div>
+          <div className="hidden xxl:flex flex-col">
+            <span className="font-bold">Frontend Dev</span>
+            <span className="text-sm text-textGray">@omejua</span>
+          </div>
+        </div>
+        <div className="hidden xxl:block cursor-pointer font-bold">...</div>
+      </div>
+    </div>
   )
 }
 
