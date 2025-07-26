156c4e3c0037b6d8b110a9c85ba96501304a1bde
Emmanuel Omejua <omejuaifeanyi2@gmail.com>
2025-07-26 07:58:14 +0000

    share component UI design

--- components/Post.tsx
+++ components/Post.tsx
@@ -2,12 +2,24 @@ import ImageComp from '@/util/ImageKit';
 import React from 'react'
 import PostInfo from './PostInfo';
 import Image from 'next/image';
+import PostInteraction from './PostInteraction';
 
 const Post = () => {
 
   return (
     <div className='p-4 border-y-[1px] border-[#2f3336]'>
         <div className="flex items-center gap-2 text-sm text-[#71767b] mb-2 font-bold">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="18"
+          height="18"
+          viewBox="0 0 24 24"
+        >
+          <path
+            fill="#71767b"
+            d="M4.75 3.79l4.603 4.3-1.706 1.82L6 8.38v7.37c0 .97.784 1.75 1.75 1.75H13V20H7.75c-2.347 0-4.25-1.9-4.25-4.25V8.38L1.853 9.91.147 8.09l4.603-4.3zm11.5 2.71H11V4h5.25c2.347 0 4.25 1.9 4.25 4.25v7.37l1.647-1.53 1.706 1.82-4.603 4.3-4.603-4.3 1.706-1.82L18 15.62V8.25c0-.97-.784-1.75-1.75-1.75z"
+          />
+        </svg>
             <span className="">
                 Emmanuel Omejua reposted
             </span>
@@ -34,8 +46,8 @@ const Post = () => {
               reprehenderit excepturi temporibus, ducimus necessitatibus fugiat
               iure nam voluptas soluta pariatur inventore.</p>
 
-              <Image src='/general/post.jpeg' alt='' width={600} height={500} />
-
+            <Image src='/general/post.jpeg' alt='' width={600} height={500} className='rounded-[15px]'/>
+            <PostInteraction/>
             </div>
         </div>
     </div>
--- components/Share.tsx
+++ components/Share.tsx
@@ -1,8 +1,87 @@
-import React from 'react'
+"use client";
+
+
+import React, { ChangeEvent, useState } from 'react';
+import Image from 'next/image';
+import { shareAction } from '@/actions';
+
+
 
 const Share = () => {
+
+    const [media, setMedia] = useState<File | null>(null);
+
+
+    const handleMediaUpload = (e: ChangeEvent <HTMLInputElement>) => {
+        if(e.target.files && e.target.files[0]){
+          setMedia(e.target.files[0])
+        }
+    }
+
+
   return (
-    <div className=''>Share</div>
+    <form className='p-4 flex gap-4' action={shareAction}> 
+
+        <div className="relative w-10 h-10 rounded-b-full overflow-hidden">
+            <Image src='/general/alan.svg' fill={true} alt='' />
+        </div>
+
+        <div className="flex-1 flex flex-col gap-4">
+          <input
+            type="text"
+            name="desc"
+            placeholder="What is happening?!"
+            className="bg-transparent outline-none placeholder:text-textGray text-xl"
+          />
+
+          <div className="flex items-center justify-between gap-4 flex-wrap">
+            <div className="flex gap-4 flex-wrap">
+            <input type="file" className="hidden" id='file' name='file' onChange={handleMediaUpload}/>
+
+            <label htmlFor='file'>
+              <Image
+                src="icons/gif.svg"
+                alt=""
+                width={20}
+                height={20}
+                className="cursor-pointer"
+              />
+            </label>
+
+            <Image
+              src="icons/poll.svg"
+              alt=""
+              width={20}
+              height={20}
+              className="cursor-pointer"
+            />
+            <Image
+              src="icons/emoji.svg"
+              alt=""
+              width={20}
+              height={20}
+              className="cursor-pointer"
+            />
+            <Image
+              src="icons/schedule.svg"
+              alt=""
+              width={20}
+              height={20}
+              className="cursor-pointer"
+            />
+            <Image
+              src="icons/location.svg"
+              alt=""
+              width={20}
+              height={20}
+              className="cursor-pointer"
+            />
+            </div>
+            <button className="bg-white text-black font-bold rounded-full py-2 px-4">Post</button>
+          </div>
+
+        </div>
+    </form>
   )
 }
 
